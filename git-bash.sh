6a164d06858cf40ff5ab3b487392b2f8f67fda9a
dinko <rocket4ce@gmail.com>
2017-01-02 16:40:14 -0300

    arreglo de bash

--- git-bash.sh
+++ git-bash.sh
@@ -5,4 +5,4 @@ test -z $repo_name && echo "El nombre de repositorio es requerido." 1>&2 && exit
 
 curl -u 'rocket4ce' https://api.github.com/user/repos -d "{\"name\":\"$repo_name\"}"
 
-git add . ; git commit -m "initial commit" ; git remote add origin "https://github.com/rocket4ce/$repo_name.git" ; git push -u origin master
+git init ; git add . ; git commit -m "initial commit" ; git remote add origin https://github.com/rocket4ce/$repo_name.git ; git push -u origin master
